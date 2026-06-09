0bf92a426770750d21fdf40f131442be8a623ee1
Siddhi Patade <patadesiddhi20@gmail.com>
2026-06-10 00:06:34 +0530

    Initial commit

--- app.py
+++ app.py
@@ -191,5 +191,8 @@ def reset():
 
     return render_template("index.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
